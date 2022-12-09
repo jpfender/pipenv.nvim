3d607a65f7399dd84376e16b1b4a5bc644bc7311
Jakob Pfender <Jakob.Pfender@MSASafety.com>
2022-12-09 16:03:21 +0100

    lualine component: do not format string

--- lua/lualine/components/pipenv.lua
+++ lua/lualine/components/pipenv.lua
@@ -1,23 +1,14 @@
 local M = require("lualine.component"):extend()
 
-local highlight = require("lualine.highlight")
-local utils = require("lualine.utils.utils")
-
 function M:init(options)
 	M.super.init(self, options)
-	self.colors = {}
-	self.color = highlight.create_component_highlight_group(
-		{ fg = utils.extract_highlight_colors("WarningMsg", "fg") },
-		"pipenv",
-		self.options
-	)
 end
 
 function M:update_status()
 	if vim.bo.filetype == "python" then
 		local venv = require("pipenv").get_current_venv()
 		if venv then
-			return string.format("%s %s", highlight.component_format_highlight(self.color), venv.name)
+			return string.format("%s", venv.name)
 		else
 			return ""
 		end
